0f1e8028e267da2601f602137d2199bca35bba2b
Guillaume BERSAC <bersac_1@hotmail.fr>
2015-01-09 17:25:29 +0100

    get to work later

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ A python prog to authomatically create c++ class
 
 It use the [Cheetah template engine](http://www.cheetahtemplate.org/index.html).
 
+If there is a folder inc, the header will be inserted in this folder.
+If there is a folder src, the source will be inserted in this folder.
 
 ## Installing on Mac OSX
 First you have to download cheetah :
@@ -13,4 +15,14 @@ python setup.py install --user
 
 ## Execution
 Just use the python command on main.py. If your program is in the ~/progs folder, adding the following alias to your [.zshrc, bashrc] will do it :
+```
 alias class="python ~/progs/cpp_class_generator/main.py"
+```
+
+## Customisation
+The templates are header.tmpl and inplementation.tmpl. Change them as you want !
+
+My modifiing the variables at the bottom of the main.py file, you can :
+* change the extension of the file
+* the target directory of the created files
+* the header guard extension
--- main.py
+++ main.py
@@ -2,11 +2,29 @@ import os
 import sys
 from Cheetah.Template import Template
 
-HEADER_EXT	= "_HEADER"
-HPP_EXT		= ".hpp"
-CPP_EXT		= ".cpp"
-SRC_FOLDER	= "src"
-INC_FOLDER	= "inc"
+HEADER_EXT		= "_HEADER"
+HPP_EXT			= ".hpp"
+CPP_EXT			= ".cpp"
+SRC_FOLDER		= "src"
+INC_FOLDER		= "inc"
+DEFAULT_TYPE	= "std::string"
+
+class Attribute:
+	"""docstring for Attribute"""
+
+	type = DEFAULT_TYPE
+	name = "name"
+
+	def __init__(self, type, name):
+		self.type = type
+		self.name = name
+
+	def getGetterName():
+		return "get" + name[0].upper() + name[1:]
+
+	def getSetterName():
+		return "set" + name[0].upper() + name[1:]
+
 
 def createFile(targetFolder, fileName, content):
 	if os.path.exists(targetFolder):
@@ -21,8 +39,21 @@ def createFile(targetFolder, fileName, content):
 	f.close()
 	print fileName + " created"
 
+def inputAttrs():
+	lstAttr
+	while true:
+		attrName = raw_input('Enter attr name: ')
+		if attrName == "":
+			return lstAttr
+		attrType = raw_input('Enter attr type: ')
+		if attrType == "":
+			attrType = DEFAULT_TYPE
+
+
+
 def main():
 	className = raw_input('Enter class name: ')
+	attrs = inputAttrs()
 
 	nameSpace = {'headerName': className.upper() + HEADER_EXT,
 			'className': className}
