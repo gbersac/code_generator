94726483c1cab2111be19bc83732f158d05528c9
Guillaume BERSAC <bersac_1@hotmail.fr>
2015-01-09 16:35:57 +0100

    create file from template

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # cpp_class_generator
 A python prog to authomatically create c++ class
+
+It use the [Cheetah template engine](http://www.cheetahtemplate.org/index.html).
+
+
+## Installing on Mac OSX
+First you have to download cheetah :
+https://pypi.python.org/pypi/Cheetah/2.4.4
+
+To install cheetah without the root rights, de-tar it, go inside the folder and run the following command :
+python setup.py install --user
+
+## Execution
+Just use the python command on main.py. If your program is in the ~/progs folder, adding the following alias to your [.zshrc, bashrc] will do it :
+alias class="python ~/progs/cpp_class_generator/main.py"
--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import os
 import sys
+from Cheetah.Template import Template
+
+HEADER_EXT	= "_HEADER"
+HPP_EXT		= ".hpp"
+CPP_EXT		= ".cpp"
+SRC_FOLDER	= "src"
+INC_FOLDER	= "inc"
+
+def createFile(targetFolder, fileName, content):
+	if os.path.exists(targetFolder):
+		fileName = targetFolder + "/" + fileName
+	#check is the file exist
+	if os.path.exists(fileName):
+		print "The file " + fileName + " already exist. Nothing done."
+		return
+	#insert the content in the file
+	f = open(fileName, "a")
+	f.write(str(content))
+	f.close()
+	print fileName + " created"
 
 def main():
 	className = raw_input('Enter class name: ')
 
-	fileName = className + ".cpp"
-	if os.path.exists("src"):
-		open("src/" + fileName, "a").close()
-		print "src/" + fileName + " created"
-	else:
-		open(fileName, "a").close()
-		print fileName + " created"
-
-	fileName = className + ".hpp"
-	if os.path.exists("inc"):
-		open("inc/" + fileName, "a").close()
-		print "inc/" + fileName + " created"
-	else:
-		open(fileName + ".hpp", "a").close()
-		print fileName + " created"
+	nameSpace = {'headerName': className.upper() + HEADER_EXT,
+			'className': className}
+	t = Template(file = sys.path[0] + "/header.tmpl", searchList=[nameSpace])
+	createFile(INC_FOLDER, className + HPP_EXT, t)
+	t = Template(file = sys.path[0] + "/implementation.tmpl", searchList=[nameSpace])
+	createFile(SRC_FOLDER, className + CPP_EXT, t)
 
 main()
