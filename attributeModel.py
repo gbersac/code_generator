5499b9b26bc5cf068ef010ac623532bc81861d41
Guillaume Bersac <bersac_1@hotmail.fr>
2015-01-13 21:24:58 +0100

    regex for attribute type ok

--- attributeModel.py
+++ attributeModel.py
@@ -1,17 +1,48 @@
 import init
+import re
+
+regexConst		= "(^|[^a-zA-Z])const([^a-zA-Z]|$)"
+regexPointer	= "\\*"
+regexReference	= "&"
+
+class PointerType:
+	NONE = 1
+	POINTER = 2
+	REFERENCE = 3
+
+def expandRegex(strg):
+	return ".*" + strg + ".*"
 
 class Attribute:
 	"""docstring for Attribute"""
 
 	aType = init.DEFAULT_TYPE
 	name = "name"
+	isConst = False
+	ptrType = PointerType.NONE
 
 	def __init__(self, _aType, _name):
-		if _aType == "":
-			self.aType = init.DEFAULT_TYPE
-		else:
-			self.aType = _aType
+		self.setType(_aType)
 		self.name = _name
+
+	def setType(self, strg):
+		if str == "":
+			self.aType = init.DEFAULT_TYPE
+			return
+		if re.match(expandRegex(regexConst), strg):
+			print "###constant"
+			self.isConst = True
+			strg = re.sub(regexConst, "", strg)
+		if re.match(expandRegex(regexPointer), strg):
+			print "###ptr"
+			self.ptrType = PointerType.POINTER
+			strg = re.sub(regexPointer, "", strg)
+		if re.match(expandRegex(regexReference), strg):
+			print "###ref"
+			self.ptrType = PointerType.REFERENCE
+			strg = re.sub(regexReference, "", strg)
+		print "result: " + strg
+		self.aType = strg.strip()
 
 	def getName(self):
 		return "_" + self.name
